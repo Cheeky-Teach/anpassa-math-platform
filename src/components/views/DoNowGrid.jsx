acbf8c03f7150c92d778ed5b20ef66c43875027d
Chuck <cmejilla@gmail.com>
2026-01-30 10:03:45 +0100

    do now grid 3 columns

--- src/components/views/DoNowGrid.jsx
+++ src/components/views/DoNowGrid.jsx
@@ -200,7 +200,7 @@ const DoNowGrid = ({ questions, ui, lang, onBack, onRefreshAll, onRefreshOne }) 
 
             {/* Scrollable Grid Area */}
             <div className="flex-1 p-4 md:p-8 overflow-y-auto">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-8xl mx-auto pb-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 max-w-8xl mx-auto pb-10">
                     {questions.map((q, i) => (
                         <DoNowCard
                             key={i}
